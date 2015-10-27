00eed93c7ab2ef40e0cb6e4278c4e8613cc3b3a4
jomorodion <omorodionjames@gmail.com>
2015-10-27 10:52:07 -0400

    rest implementation

--- LDS/src/com/lds/LDS.java
+++ LDS/src/com/lds/LDS.java
@@ -1,18 +1,48 @@
 package com.lds;
 
-import javax.jws.WebService;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
 
 import com.lds.stubs.Person;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
 
-@WebService
+@Path("/LDS")
 public class LDS {
 	
-	public String getZip(String lng, String lat)
+	public Client client;
+	public WebResource webResource;
+//	https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452
+	public final String GOOGLE_GEOLOC_LONG_LAT = "https://maps.googleapis.com/maps/api/geocode/json?latlng=";
+	
+	public LDS()
 	{
-		String zip = "";
+		
+	}
+	
+	
+	@GET
+	@Path("/test/{param}")	
+	public String getTest(@PathParam("param") String param)
+	{		
+		return param;
+	}
+	
+	@GET
+	@Path("/get/long/lat/{lng}/{lat}")
+	public String getZip(@PathParam("lng")String lng, @PathParam("lat")String lat)
+	{
+		System.out.println("**************Lng and Lat: " + lng + "," + lat);
+		String response = "Empty";
+		client = Client.create();
+		webResource = client.resource(GOOGLE_GEOLOC_LONG_LAT + lng + "," + lat);
+		
+		response = webResource.type(MediaType.APPLICATION_JSON).get(String.class);
 		
 		
-		return zip;
+		return response;
 	}
 	
 	public boolean setLocation(String lng, String lat)
---
+++ LDS/src/com/lds/stubs/google/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Contains stubs for Google api request/response.
+ */
+/**
+ * @author jomorodion
+ *
+ */
+package com.lds.stubs.google;
